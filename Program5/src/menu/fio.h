ad87132981e592242bc53c308e776caf3a1d5919
Michelle Yahav <mtik.philosopher@gmail.com>
2023-08-06 12:07:44 -0700

    added starter interface for file interaction in fio files

--- Program5/src/menu/fio.h
+++ Program5/src/menu/fio.h
@@ -1,5 +1,22 @@
 #ifndef FIO_H_
 #define FIO_H_
+#include <stdio.h>
+#define PATH_SIZE 100
+
+static char path[PATH_SIZE];
+//in case the user entered a non existant file location the function will create it automatically.
+void save_file_create(){
+ FILE* stream = fopen(path, "w+");
+ if (stream)
+   fclose(stream);
+}
+
+// this function has to be called in the beginning before upload or in the end before save.
+void ask_for_save_file_location(){
+ puts("Enter the path for save file");
+ scanf("%s", path);
+}
+
 
 
 
