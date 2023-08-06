0e3a1f4ced9b04560ec948cc33874bca426aeebb
Michelle Yahav <mtik.philosopher@gmail.com>
2023-08-06 12:15:59 -0700

    fixed .c/.h file relation in fio

--- Program5/src/menu/fio.c
+++ Program5/src/menu/fio.c
@@ -1,1 +1,14 @@
 #include "fio.h"
+#include <stdio.h>
+static char path[PATH_SIZE];
+ 
+void save_file_create(){
+ FILE* stream = fopen(path, "w+");
+ if (stream)
+   fclose(stream);
+}
+ 
+void ask_for_save_file_location(){
+ puts("Enter the path for save file");
+ scanf("%s", path);
+}
--- Program5/src/menu/fio.h
+++ Program5/src/menu/fio.h
@@ -1,21 +1,11 @@
 #ifndef FIO_H_
 #define FIO_H_
-#include <stdio.h>
-#define PATH_SIZE 100
-
-static char path[PATH_SIZE];
+ 
 //in case the user entered a non existant file location the function will create it automatically.
-void save_file_create(){
- FILE* stream = fopen(path, "w+");
- if (stream)
-   fclose(stream);
-}
+void save_file_create();
 
 // this function has to be called in the beginning before upload or in the end before save.
-void ask_for_save_file_location(){
- puts("Enter the path for save file");
- scanf("%s", path);
-}
+void ask_for_save_file_location();
 
 
 
