3e96accc2f96f66edfefc68009b45b325d9c2b7e
MarikTik <mtik.philosopher@gmail.com>
2024-02-23 22:23:44 -0800

    implemented simpson's rule

---
+++ src/approximation_methods/simpsons_rule.py
@@ -0,0 +1,14 @@
+from typing import Callable
+
+function = Callable[[float], float] # type alias for mathematical function signature
+
+def simpsons_approximation(f: function, a: float, b: float, n: int) -> float:
+    delta_x = (b - a) / n
+    x0 = a
+    x = lambda i : x0 + i * delta_x
+    return delta_x / 3 * (
+             f(x0) + 
+             2 * sum(f(x(2 * i)) for i in range(1, n // 2)) + 
+             4 * sum(f(x(2 * i - 1)) for i in range(1, n // 2 + 1)) +
+             f(x(n))
+          )
