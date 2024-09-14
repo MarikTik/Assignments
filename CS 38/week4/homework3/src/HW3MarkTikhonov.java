db67f45c66ec1d5ba2448e34c11c6c0ace95fe1c
MarikTik <mtik.philosopher@gmail.com>
2024-09-13 22:35:38 -0700

    added more comments to homework3 and mild additions to code

--- CS 38/week4/homework3/src/HW3MarkTikhonov.java
+++ CS 38/week4/homework3/src/HW3MarkTikhonov.java
@@ -36,12 +36,17 @@ public class HW3MarkTikhonov {
         }
     }
 
-   
+    /**
+    * returns a standardized screen form employing a given set settings.
+    *
+    * @param settings the desired settings that the screen should abide by
+    * @return Draw object with the given settings
+    */
     public static Draw standardScreen(ScreenSettings settings){
         Draw screen = new Draw();
 
         //scaling the screen
-        screen.setCanvasSize(settings.width, settings.height);
+        screen.setCanvasSize(settings.width, settings.height); 
         screen.setXscale(0, settings.width);
         screen.setYscale(settings.height, 0);
         screen.setTitle(settings.title);
@@ -49,32 +54,47 @@ public class HW3MarkTikhonov {
 
         screen.setPenColor(Draw.BLACK);  
         screen.setFont(settings.font);  
-        //screen.text(halfWidth, nameFont.getSize(), "Mark Tikhonov", rotation);
-
-        //drawing delimter lines as a proof of concept
-        screen.line(settings.width / 2, settings.height, settings.width / 2, 0);
-        screen.line(0, settings.height / 2, settings.width, settings.height / 2);
+     
+    
+        screen.line(settings.width / 2, settings.height, settings.width / 2, 0); // draws a cross separating the screen into 
+        screen.line(0, settings.height / 2, settings.width, settings.height / 2); // 4 sections
         
         screen.setPenColor(settings.penColor);
         return screen;
     }
 
-    public static Draw writeLines(Draw screen, int x, int y, String []lines){
-        int yOffset = screen.getFont().getSize();
+    /**
+     * writes an array of lines separated by vertical jumps on the screen canvas starting from (x, y)
+     * writes from top to bottom
+     * 
+     * @param screen the screen object to write lines on 
+     * @param x left horizontal coordinate for the beginning of the text
+     * @param y left vertical coordinate for the beginning of the text
+     * @param lines an array of strings to print on the canvas 
+     * @return none
+     */
+    public static void writeLines(Draw screen, int x, int y, String []lines){
+        int yOffset = screen.getFont().getSize(); 
         y += yOffset;
         for (var line : lines){ // I have seen this in the quiz so it's reasonable to use it
              screen.textLeft(x, y, line);
              y += yOffset;
         }
-
-        return screen;
     }
 
-    // accepts newlines as a way to split a single line into pieces
-    public static Draw writeLine(Draw screen, int x, int y, String line){
+    /**
+     * writes a string on the screen canvas respecting newline characters "\n" in the given string
+     * @param screen the screen object to write lines on 
+     * @param x left horizontal coordinate for the beginning of the text
+     * @param y left vertical coordinate for the beginning of the text
+     * @param line the string to print on canvas
+     * @return none
+     *
+     */
+    public static void writeLine(Draw screen, int x, int y, String line){
         writeLines(screen, x, y, line.split("[\n]"));
-        return screen;
     }
+
     // this interface will be used as a placeholder for lambda expressions of the form T expression(T val1, T val2)
     // so that everything that looks like it will fit nicely.
     interface Comparator<T>{
@@ -87,8 +107,8 @@ public class HW3MarkTikhonov {
     public static <T> void sortArray(T []array, Comparator<T> comparator){
         for (int i = 0; i < array.length; i++)
             for (int j = i + 1; j < array.length; j++)
-                if (!comparator.compare(array[i], array[j])){
-                    T temp = array[i];
+                if (!comparator.compare(array[i], array[j])){ // for any pair of elements, if the comparator returns false, swap them
+                    T temp = array[i]; 
                     array[i] = array[j];
                     array[j] = temp;
                 }
@@ -108,8 +128,8 @@ public class HW3MarkTikhonov {
     public static void romanNumerals(){
         System.out.print("Enter roman numeral in the range of 1-10 (I - X): ");
         Scanner scanner = new Scanner(System.in);
-        String numeral = scanner.nextLine();
-        Draw screen = standardScreen(
+        String numeral = scanner.next(); // taking only the next token so that to limit user errors
+        Draw screen = standardScreen( // setting my screen
             new ScreenSettings(
                 400,
                 400,
@@ -118,8 +138,8 @@ public class HW3MarkTikhonov {
                 new Font("Serif", Font.BOLD, 24),
                 "romanNumerals by Mark Tikhonov"
         ));
-
-        int result = switch(numeral){
+        // switch statement as shown in the book, maps the strings to return values and assignes to result
+        int result = switch(numeral){ 
             case "I" -> 1;
             case "II" -> 2;
             case "III" -> 3;
@@ -133,11 +153,11 @@ public class HW3MarkTikhonov {
 
             default -> -1;
         };
-        if (result == -1) {
+        if (result == -1) { // if there was a mistake, notify the user
             System.out.println("Error: Numeral is not in range 1 - 10");
             writeLine(screen, 16, 16, "Error:\nNumeral is\nnot in \nrange\n1-10");
         }
-        else {
+        else { // if everything is good, output the translation of the roman numeral to the console/graphics screen
             System.out.printf("%s = %d\n", numeral, result);
             screen.textLeft(16, 16, numeral + " = " + result);
         }
@@ -158,44 +178,61 @@ public class HW3MarkTikhonov {
         ));
 
         System.out.println("Enter three names to be sorted in ascending order: ");
-        
-        for (int i = 0; i < 3; i++)
+        // iterating to scan all the names into an array, this is better because I can change the amount of names 
+        // in the function as I desire.
+        for (int i = 0; i < names.length; i++)
             names[i] = scanner.next();
         
+        // this came from more functional based approach which might be foreign to Java's OOP structure
+        // the idea is that for each pair of names, I am going to comapre them and if the function (which
+        // here is represented by an anonymous lambda) returns false, the sortArray method swaps any two names
+        // in this way I can sort by any desired property at my whim, which is much more convenient.
         sortArray(names, (name1, name2) -> name1.compareTo(name2) < 0); 
+
         // amazing that it works even though I gave a pretty random name for the interface method "compare"
         // apparently for this kind of interface only the signature matters and not the name, 
         // my guess is that it is true only for interfaces with one method, though I may be wrong.
 
-        for (var name : names)
-            System.out.println(name);
-        writeLines(screen, 16, 16, names);
-        scanner.close();
-        // echo "Charlie Leslie Andy" | java week4/homework3/src/HW3MarkTikhonov.java
+        for (int i = 0; i < names.length; i++){
+            names[i] = (i + 1) + ". " + names[i]; // adding positional identifiers to the name
+            System.out.println(names[i]);
+           
+        }  
+            
+        writeLines(screen, 16, 16, names); // outputing the ordered array on the graphics screen
+        scanner.close(); // not forget to clean the mess...
     }
 
     public static void runningTheRace(){
-        String []horsemen = new String[3];
-        double []raceTimes = new double[3];
+        //here I select a simillar approach as in the previous method, this time with two arrays
+        int horsemenNumber = 3;
+        String []horsemen = new String[horsemenNumber];
+        double []raceTimes = new double[horsemenNumber];
         Scanner scanner = new Scanner(System.in);
-        Draw screen = standardScreen(
+        Draw screen = standardScreen( // setting my screen
             new ScreenSettings(
                 400,
                 400,
                 Draw.GRAY,
                 Draw.RED, 
                 new Font("Ariel", Font.BOLD, 24),
-                "runningTheRace by Mark Tikhonov"
+                "runningTheRace by Mark Tikhonov" 
         ));
 
         System.out.println("Enter horseman racers names and the time it took tham to complete the race in the format: [name] [time]");
-        for(int i = 0; i < 3; i++){
+        // iterates over the minimum size of the two arrays, here they should have
+        // length of 3, but if I will decide to change one and forget the other, I will be
+        // shielded from an out of bounds error.
+        for(int i = 0; i < Math.min(horsemen.length, raceTimes.length); i++){
             horsemen[i] = scanner.next();
             raceTimes[i] = scanner.nextDouble();
         }
-        // please take into account that I am relying purely on my experience with C# and other languages, 
-        // It's the code I have written after clarifying some of my assumptions on the internet (no AI used).
 
+        // this has a simillar idea as what I used in the previous function.
+        // Though it looks complext, it really isn't, what it does is sort by the
+        // position of the racer in the raceTimes array, the position is exactly
+        // the index of a given horseman at reflected in the raceTimes array.
+        // This method call illustrates this concept.
         sortArray(horsemen, (horseman1, horseman2) -> { 
             int index1 = indexOf(horsemen, horseman1);
             int index2 = indexOf(horsemen, horseman2);
@@ -203,23 +240,22 @@ public class HW3MarkTikhonov {
             return raceTimes[index1] < raceTimes[index2];
         });
         
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < horsemen.length; i++){ // iterate over the sorted horsemen list and output the positions
             System.out.printf("%d. %s\n", i + 1, horsemen[i]);
         }
-        writeLines(screen, 16, 16, new String[]{
-            "1. " + horsemen[0],
-            "2. " + horsemen[1],
-            "3. " + horsemen[2]
+        writeLines(screen, 16, 16, new String[]{ // print the positions of the first 3 horsemen
+            "1. " + horsemen[0], // this satisfies the requirements of the assignment, however if another number would be
+            "2. " + horsemen[1], // chosen, it would be problematic, for n > 3 the program would show only 3 names but 
+            "3. " + horsemen[2] // for n < 3, the program would crash 
         });
-        //echo "Charlie 3 Leslie 10 Andy 1" | java week4/homework3/src/HW3MarkTikhonov.java
-        scanner.close();
+        scanner.close(); // cleaning again...
     }
 
 
     public static void main(String []args){
         //romanNumerals();
-        //sortedNames();
-        runningTheRace();
+        sortedNames();
+        //runningTheRace();
         
         
     }
