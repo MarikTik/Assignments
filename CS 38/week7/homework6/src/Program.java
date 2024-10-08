cc576c161a1086c89a2166cc37584f90a8355b00
MarikTik <mtik.philosopher@gmail.com>
2024-10-08 10:27:47 -0700

    updating latest homework

--- CS 38/week7/homework6/src/Program.java
+++ CS 38/week7/homework6/src/Program.java
@@ -1,7 +1,5 @@
 //package week7.homework6.src;
-import java.awt.Color;
 import java.awt.Font;
-import java.util.Scanner;
 import java.util.Random;
 
 /**
@@ -12,37 +10,6 @@ import java.util.Random;
 * @version 0.0.1
 */
 public class Program {
-
-    private static class Form{
-        public int width;
-        public int height;
-        public Color backgroundColor;
-        public Color penColor;
-        public Font font;
-        public String title;
-
-        public Form(int width, int height, Color backgroundColor, Color penColor, Font font, String title){
-            this.width = width;
-            this.height = height;
-            this.backgroundColor = backgroundColor;
-            this.penColor = penColor;
-            this.font = font;
-            this.title = title;
-        }
-        public Draw create(){
-            Draw screen = new Draw();
-            //scaling the screen
-            screen.setCanvasSize(width, height);
-            screen.setXscale(0, width);
-            screen.setYscale(height, 0);
-            screen.setTitle(title);
-            screen.clear(backgroundColor);
-            screen.setFont(font);  
-            screen.setPenColor(penColor);
-            return screen;
-        }
-    }
-    
     private static final int width = 400, height = 400;
     private static Random rand = new Random();
 
@@ -115,7 +82,7 @@ public class Program {
         System.out.println(message);
     }
     public static void main(String [] args) {
-       //drawFlashLight();
+       drawFlashLight();
        diceGame();
     }
 }
